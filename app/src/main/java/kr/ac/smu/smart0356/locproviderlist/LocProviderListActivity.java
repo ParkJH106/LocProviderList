8ef4366350767a32e2973e01db753722c9e0da16
ParkJH106 <jeongha4209@naver.com>
2021-04-07 15:14:51 +0900

    added button

--- app/src/main/java/kr/ac/smu/smart0356/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/kr/ac/smu/smart0356/locproviderlist/LocProviderListActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class LocProviderListActivity extends AppCompatActivity {
+public class LocProviderListActivity extends AppCompatActivity implements View.OnClickListener{
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,15 @@ public class LocProviderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.txtOutput);
+        mbutton = findViewById(R.id.button);
         locMgr = (LocationManager) getSystemService((LOCATION_SERVICE));
 
+        mbutton.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onClick(View v) {
         locProviders = locMgr.getAllProviders();
         String s = "";
         for(int i = 0; i < locProviders.size(); i++) {
